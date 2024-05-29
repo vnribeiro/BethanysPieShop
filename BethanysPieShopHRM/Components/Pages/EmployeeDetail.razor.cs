fe0c7fdf0e008f9c6dc64a01e93bbc705d0b6fe9
vnribeiro <viniciuscostaa.ribeiro@outlook.com>
2024-05-29 19:56:33 -0300

    initial commit

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,24 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override void OnInitialized()
+        {
+            Employee = MockDataService.Employees!.Single(e => e.EmployeeId == EmployeeId);
+        }
+
+        private void ChangeHolidayState()
+        {
+            Employee.IsOnHoliday = !Employee.IsOnHoliday;
+        }
+    }
+}
--- BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -5,11 +5,11 @@ namespace BethanysPieShopHRM.Components.Pages
 {
     public partial class EmployeeOverview
     {
-        public List<Employee> Employees { get; set; } = default!;
+        public List<Employee>? Employees { get; set; } = default!;
 
         protected override async Task OnInitializedAsync()
         {
-            await Task.Delay(2000);
+            await Task.Delay(500);
             Employees = MockDataService.Employees;
         }
     }
