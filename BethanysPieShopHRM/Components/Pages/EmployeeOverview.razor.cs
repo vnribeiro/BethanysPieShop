1d51904556355d6f09c393ea786b1d8c959a7ee0
vnribeiro <viniciuscostaa.ribeiro@outlook.com>
2024-05-29 18:41:26 -0300

    Add project files.

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Components.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee> Employees { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(2000);
+            Employees = MockDataService.Employees;
+        }
+    }
+}
